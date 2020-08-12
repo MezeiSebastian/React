edbce7ee18ac88337f693086e9ec9d92b08f0287
MezeiSebastian <61548741+MezeiSebastian@users.noreply.github.com>
2020-08-12 20:38:28 +0300

    React app with terraUI

---
+++ homework/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// eslint-disable-next-line
+import logo from './logo.svg';
+import './App.css';
+import DefaultTable from './components/table-component/table.component.component';
+import NoStripedTable from './components/header-component/striped-table-component.component'
+
+function App() {
+  return (
+    
+    <div className="App">
+        <DefaultTable/>
+   
+    </div>
+  );
+}
+
+export default App;
